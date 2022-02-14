0d6e6ff08edbc241ec79a7b6e89d069414f992e6
Jeong-w-o-o <irmnels@gmail.com>
2022-02-15 03:29:54 +0900

    [FEAT] 이메일 인증: 도메인 추가

---
+++ src/main/java/com/commentdiary/src/emailAuth/domain/EmailAuth.java
@@ -0,0 +1,26 @@
+package com.commentdiary.src.emailAuth.domain;
+
+import com.commentdiary.common.domain.BaseTimeEntity;
+import com.commentdiary.src.member.domain.enums.MemberStatus;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "email_auth")
+public class EmailAuth extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(length = 45, nullable = false)
+    private String email;
+
+    @Column(length = 4, nullable = false)
+    private int authNumber;
+}
